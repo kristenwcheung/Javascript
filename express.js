91fa4cd0d8d6522bc4036c457a44b029b75853f8
Kristen <105521957+kristenwcheung@users.noreply.github.com>
2022-07-12 14:10:51 -0400

    updated codes

--- express.js
+++ express.js
@@ -14,7 +14,17 @@ const app = express()  //e
 // '/' home page
 
 app.get('/', (req, res) => {
-    res.send('This is the homepage!')
+    res.send('This is the homepage!!!')
+})
+
+app.get('/r/:subreddit', (req, res) => {  //patterns; ':' means 
+    const { subreddit, postId } = req.params;
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {  //patterns; ':' means 
+    const { subreddit, postId } = req.params;
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`)
 })
 
 app.get('/cats', (req, res) => {  //callback will run when the request received matching '/cats'
@@ -29,9 +39,18 @@ app.get('/dogs', (req, res) => {
     res.send('WOOF!!')
 })
 
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    if(!q){
+        res.send('NOTHING FOUND IF NOTHING SEARCHED!')
+    }
+    res.send(`<h1>Search results for: ${q}</h1>`)
+})
+
 app.get('*', (req, res) => {  //  * means anything
     res.send(`I dont' know that path!`)
 })
+
 
 app.listen(3000, () => {  //server
     console.log('LISTENING ON PORT 3000')
