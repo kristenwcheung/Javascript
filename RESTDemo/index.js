14c1b8c040c4bed3d1fe9761b5fa6403bab3057b
Kristen <105521957+kristenwcheung@users.noreply.github.com>
2022-07-21 13:00:14 -0400

    used method-override

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -2,15 +2,17 @@ const { text } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
 const { v4: uuid } = require('uuid');
 uuid();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -53,11 +55,26 @@ app.get('/comments/:id', (req, res) => {
     res.render('comments/show', {comment});
 })
 
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id); 
+    res.render('comments/edit', { comment });
+})
+
+
 app.patch('/comments/:id', (req, res) => {
     const { id } = req.params;
     const newCommentText = req.body.comment;
     const foundComment = comments.find(c => c.id === id);
     foundComment.comment = newCommentText;
+    res.redirect('/comments')
+})
+
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const foundComment = comments.find(c => c.id === id);
+    comments = comments.filter(c => c.id !== id)  //it returns a new filtered array
     res.redirect('/comments')
 })
 
